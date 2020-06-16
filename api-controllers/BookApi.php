5763bbddc9a6102e94d863d2744171fbd2d5b163
Neha Jain <njain@pridigital.com>
2020-06-16 15:45:08 +0530

    codeigniter - api controllers from Books Management Project

---
+++ api-controllers/BookApi.php
@@ -0,0 +1,132 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Books Management Module
+ */
+class Book extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Method used to get the list of books available in the database.
+     */
+	public function index()
+	{
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'GET'){
+			json_output(400,array('status' => 400,'message' => 'Bad request.'));
+		} else {
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        	$response = $this->MyModel->auth();
+		        	if($response['status'] == 200){
+		        		$resp = $this->MyModel->book_all_data();
+	    				json_output($response['status'],$resp);
+		        	}
+			}
+		}
+	}
+
+    /**
+     * Method used to get the details of a book based on id
+     * Parameters: Book ID is required $id
+     */
+	public function detail($id)
+	{
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'GET' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE){
+			json_output(400,array('status' => 400,'message' => 'Bad request.'));
+		} else {
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        	$response = $this->MyModel->auth();
+		        	if($response['status'] == 200){
+		        		$resp = $this->MyModel->book_detail_data($id);
+					json_output($response['status'],$resp);
+		        	}
+			}
+		}
+	}
+    
+    /**
+     * Method used to create a new book in the database.
+     */
+	public function create()
+	{
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'POST'){
+			json_output(400,array('status' => 400,'message' => 'Bad request.'));
+		} else {
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        	$response = $this->MyModel->auth();
+		        	$respStatus = $response['status'];
+		        	if($response['status'] == 200){
+					$params = json_decode(file_get_contents('php://input'), TRUE);
+					if ($params['title'] == "" || $params['author'] == "") {
+						$respStatus = 400;
+						$resp = array('status' => 400,'message' =>  'Title & Author can\'t empty');
+					} else {
+		        			$resp = $this->MyModel->book_create_data($params);
+					}
+					json_output($respStatus,$resp);
+		        	}
+			}
+		}
+	}
+
+    /**
+     * Method used to update the details of a book based on id
+     * Parameters: Book ID is required $id
+     */
+	public function update($id)
+	{
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'PUT' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE){
+			json_output(400,array('status' => 400,'message' => 'Bad request.'));
+		} else {
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        	$response = $this->MyModel->auth();
+		        	$respStatus = $response['status'];
+				if($response['status'] == 200){
+					$params = json_decode(file_get_contents('php://input'), TRUE);
+					$params['updated_at'] = date('Y-m-d H:i:s');
+					if ($params['title'] == "" || $params['author'] == "") {
+						$respStatus = 400;
+						$resp = array('status' => 400,'message' =>  'Title & Author can\'t empty');
+					} else {
+		        			$resp = $this->MyModel->book_update_data($id,$params);
+					}
+					json_output($respStatus,$resp);
+		       		}
+			}
+		}
+	}
+
+    /**
+     * Method used to delete the book based on id
+     * Parameters: Book ID is required $id
+     */
+	public function delete($id)
+	{
+		$method = $_SERVER['REQUEST_METHOD'];
+		if($method != 'DELETE' || $this->uri->segment(3) == '' || is_numeric($this->uri->segment(3)) == FALSE){
+			json_output(400,array('status' => 400,'message' => 'Bad request.'));
+		} else {
+			$check_auth_client = $this->MyModel->check_auth_client();
+			if($check_auth_client == true){
+		        	$response = $this->MyModel->auth();
+		        	if($response['status'] == 200){
+		        		$resp = $this->MyModel->book_delete_data($id);
+					json_output($response['status'],$resp);
+		        	}
+			}
+		}
+	}
+
+}
